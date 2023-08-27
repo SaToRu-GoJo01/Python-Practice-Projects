5d8f31e1bc0a779f8de7ff4b063dd47370d1e5ba
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2023-08-27 12:29:32 +0530

    Update BankersRoulette.py

--- Projects/Who will pay the bill/BankersRoulette.py
+++ Projects/Who will pay the bill/BankersRoulette.py
@@ -1,4 +1,6 @@
 import random 
 str = input("Write the names of your friends who are going to pay the bill ->")
 result = str.split(",")
-print(result[int(random.random()*len(result))]+" is going to pay the bill.")
+randomChoice = random.randint(0,len(result)-1)
+# print(result[int(random.random()*len(result))]+" is going to pay the bill.")
+print(result[randomChoice] + " is going to pay the bill.")
