9b8f3bf3de02875c8329d58e8c9c2da8b5bc87d5
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2023-09-03 17:27:37 +0530

    Delete Practice Lectures directory

--- Practice Lectures/41RandomModule.py
+++
@@ -1,5 +0,0 @@
-import random
-
-print(random.randint(-155,100))
-
-print(random.random()*5)
--- Practice Lectures/43List.py
+++
@@ -1,4 +0,0 @@
-friends = ["Manas","Manas1","Arnav","Akshat"]
-print(friends[1])
-print("Hello World!")
-print(friends)
