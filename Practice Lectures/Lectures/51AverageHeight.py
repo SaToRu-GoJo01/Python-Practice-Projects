2c9919d277bf3e74f93279d927e6af980e5ea6c1
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2023-09-03 17:27:14 +0530

    Delete Practice Lectures/Lectures directory

--- Practice Lectures/Lectures/51AverageHeight.py
+++
@@ -1,8 +0,0 @@
-str = input("Enter the height of the students->")
-heights = [int(x) for x in str.split()]
-sum = 0
-count = 0
-for i in heights:
-    sum = sum + i
-    count = count + 1
-print("The average height of the students =" , (sum/count))
--- Practice Lectures/Lectures/53RangeInForLoop.py
+++
@@ -1,6 +0,0 @@
-for i in range(1,101,5):
-    print(i)
-total = 0
-for i in range(1,101):
-    total += i
-print(total)
